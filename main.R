96e9d410151a598e842c86a1ee6b2575f57f3e0a
unknown <blok029@D0117105.wurnet.nl>
2015-01-09 17:39:07 +0100

    First version of the industrial railway assignment, needs to be revised.

---
+++ main.R
@@ -0,0 +1,16 @@
+# Michiel Blok and Madeleine van Winkel
+# 9 January 2015
+
+# Set working directory
+#setwd()
+
+# Import packages
+library(sp)
+library(rgdal)
+library(rgeos)
+
+# Source functions
+source('R/industrialRailway.R')
+
+# Then the actual commands
+industrialRailway()
